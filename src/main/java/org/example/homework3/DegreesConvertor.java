418430bade2ff2454a8eb13c60abb816d23cff6a
hordiienkod <dmytro.hordiienko@appflame.com>
2024-03-21 23:19:39 +0200

    Squashed commit of homework3

---
+++ src/main/java/org/example/homework3/DegreesConvertor.java
@@ -0,0 +1,13 @@
+package org.example.homework3;
+
+import java.text.DecimalFormat;
+
+public class DegreesConvertor {
+    public void degreesConvertor(double celsius) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println(celsius + " Celsius degrees" + " is equal to " + decimalFormat.format(fahrenheit) + " Fahrenheit degrees.");
+        double kelvin = celsius + 273.15;
+        System.out.println(celsius + " Celsius degrees" + " is equal to " + decimalFormat.format(kelvin) + " Kelvin degrees.");
+    }
+}
