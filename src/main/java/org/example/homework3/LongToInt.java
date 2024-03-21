e9cd65c8d3e762ecfaae6857d4a459607db6ccd5
hordiienkod <dmytro.hordiienko@appflame.com>
2024-03-21 23:04:14 +0200

    Reverted data

--- src/main/java/org/example/homework3/LongToInt.java
+++
@@ -1,9 +0,0 @@
-package org.example.homework3;
-
-public class LongToInt {
-    public void longToInt(long a){
-        System.out.println("Long value: " + a);
-        int longToIntValue = (int) a;
-        System.out.println("Long value to int : " + longToIntValue);
-    }
-}
