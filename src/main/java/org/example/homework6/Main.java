c17b9e81d12d160e0de50faef05aeccb8c9f9bee
hordiienkod <dmytro.hordiienko@appflame.com>
2024-04-03 14:21:10 +0300

    homework №6 (edited)

---
+++ src/main/java/org/example/homework6/Main.java
@@ -0,0 +1,8 @@
+package org.example.homework6;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator simpleCalculator = new Calculator();
+        simpleCalculator.calculate();
+    }
+}
